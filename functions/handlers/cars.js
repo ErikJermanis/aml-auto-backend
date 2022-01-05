ba4a4a4935e78f216116408f2252c14be24b15ed
Erik Jermaniš <erik55jermanis@gmail.com>
2022-01-05 23:19:23 +0100

    car manip system done

--- functions/handlers/cars.js
+++ functions/handlers/cars.js
@@ -12,11 +12,32 @@ exports.getCars = (req, res) => {
     .catch(err => console.log(err));
 };
 
+exports.getSingleCar = (req, res) => {
+  db
+    .doc(`cars/${req.params.id}`)
+    .get()
+    .then(snapshot => {
+      if(!snapshot.exists) {
+        return res.status(404).json({ error: 'car not found' });
+      } else {
+        return res.json({ id: snapshot.id, ...snapshot.data() });
+      }
+    });
+};
+
 exports.createCar = (req, res) => {
   db
     .collection('cars')
-    .add({ ...req.body, createdAt: new Date().toISOString() })
+    .add({ ...req.body, createdAt: new Date().toISOString(), updatedAt: new Date().toISOString() })
     .then(doc => res.json({ message: `document ${doc.id} created successfully` }))
+    .catch(err => res.status(500).json({ error: 'something went wrong' }));
+};
+
+exports.updateCar = (req, res) => {
+  db
+    .doc(`cars/${req.params.id}`)
+    .update({ ...req.body, updatedAt: new Date().toISOString() })
+    .then(() => res.json({ message: 'car updated successfully' }))
     .catch(err => res.status(500).json({ error: 'something went wrong' }));
 };
 
--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,14 @@ const app = require('express')();
 const cors = require('cors');
 
 const checkToken = require('./utils/authMiddleware');
-const { getCars, createCar, deleteCar } = require('./handlers/cars');
+const { getCars, getSingleCar, createCar, deleteCar, updateCar } = require('./handlers/cars');
 
 app.use(cors());
 
 app.get('/cars', getCars);
+app.get('/cars/:id', checkToken, getSingleCar);
 app.post('/cars', checkToken, createCar);
+app.put('/cars/:id', checkToken, updateCar);
 app.delete('/cars/:id', checkToken, deleteCar);
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
